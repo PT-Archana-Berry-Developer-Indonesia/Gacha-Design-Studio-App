59a37568c2824618405445ffd390556edc5b7637
Archana Berry OPS!!! <archanaberry101originally@gmail.com>
2024-12-17 00:47:01 +0000

    Menambahkan LOG ditiap program jalan...

--- app/src/main/java/com/lunime/githubcollab/archanaberry/gachadesignstudio/GachaStudioMain.kt
+++ app/src/main/java/com/lunime/githubcollab/archanaberry/gachadesignstudio/GachaStudioMain.kt
@@ -117,11 +117,10 @@ class GachaStudioMain : Activity() {
         MANIFEST_RESOURCE = baseDir.plus(APP_FOLDER).plus("/manifest.json")
         DEST_DIR_PATH = baseDir.plus(APP_FOLDER).plus("/")
         
-        
-        
         super.onCreate(savedInstanceState)
         
         setContentView(R.layout.gachastudio_welcome)
+        GachaStudioLogger.log("Sedang menginisialisasi aktivitas utama Gacha Design Studio...")
         
         val agreementAccepted = isAgreementAccepted()
 
@@ -129,27 +128,35 @@ if (agreementAccepted) {
     // Periksa apakah pembaruan diperlukan sebelum masuk ke GachaStudio
     if (checkForUpdates()) {
         // Jika pembaruan diperlukan, tampilkan dialog pembaruan
-        showUpdateDialog()
+        GachaStudioLogger.log("Memeriksa pembaruan...")
+        if (showUpdateDialog()) {
+            GachaStudioLogger.log("Perbaruan di temukan!")
+        }
     } else {
         // Jika tidak ada pembaruan, lanjutkan ke GachaStudio
         openGachaDesignStudio()
+        GachaStudioLogger.log("Membuka Gacha Design Studio...")
     }
 } else {
     // Jika perjanjian belum diterima, tampilkan dialog sambutan
     showWelcomeDialog()
+    GachaStudioLogger.log("Aku ingin menyapa :3")
 	}
 }
     
     private fun openGachaDesignStudio() {
     val intent = Intent(this, GachaStudio::class.java)
     startActivity(intent)
-    finish() // Tutup aktivitas saat ini
+    if (finish()) {
+        GachaStudioLogger.log("Aku hindarkan aktivitas utama karena ga ada apa apa, KOSONG!")
+    } // Tutup aktivitas saat ini
 }
 
         // Fungsi untuk mendeteksi direktori dinamis
     private fun detectDynamicDirectory(): String {
     // Deteksi apakah pengguna adalah user pertama (0) atau lainnya
     val userId = android.os.Process.myUserHandle().hashCode() // Unik untuk tiap pengguna
+    GachaStudioLogger.log("Menggunakan penyimpanan di pengguna $userId")
     return if (userId == 0) {
         "/storage/emulated/0"
     } else {
@@ -172,9 +179,17 @@ if (agreementAccepted) {
                 val eElement = nList.item(0) as Element
                 val accepted = eElement.getElementsByTagName("accepted").item(0).textContent
                 java.lang.Boolean.parseBoolean(accepted)
+                GachaStudioLogger.log("Yey, persetujuan nya diterima :3")
             }
         } catch (e: Exception) {
+                // Mengubah stacktrace menjadi String
+            val stackTraceWriter = StringWriter()
+            e.printStackTrace(PrintWriter(stackTraceWriter))
+            val stackTraceAsString = stackTraceWriter.toString()
+
             e.printStackTrace()
+            GachaStudioLogger.log("Yah persetujuan nya ditolak T wT")
+            GachaStudioLogger.log("Yah kok aku ditolak sih? T wT, Dengan pesan ditolak $stackTraceAsString", isError = true)
             false
         }
     }
@@ -184,7 +199,9 @@ if (agreementAccepted) {
             val configFile = File(CONFIG_FILE_PATH)
             if (!configFile.exists()) {
                 configFile.parentFile.mkdirs()
+                GachaStudioLogger.log("Membuat folder konfigurasi...")
                 configFile.createNewFile()
+                GachaStudioLogger.log("Menulis file konfigurasi...")
             }
 
             val dbFactory = DocumentBuilderFactory.newInstance()
@@ -208,13 +225,16 @@ if (agreementAccepted) {
             val source = DOMSource(doc)
             val result = StreamResult(FileOutputStream(configFile))
             transformer.transform(source, result)
+            GachaStudioLogger.log("Berhasil membuat file konfigurasi")
         } catch (e: Exception) {
+            GachaStudioLogger.log("Gagal membuat file konfigurasi!", isError = true)
             e.printStackTrace()
         }
     }
 
         private fun showWelcomeDialog() {
         val builder = AlertDialog.Builder(this)
+        GachaStudioLogger.log("Memanggil dialog sambutan selamat datang :3 ...")
         builder.setTitle("Selamat Datang di Gacha Design Studio!")
         builder.setMessage(
             "Gacha Design Studio hadir dengan bebas berkreasi membuat karakter Gacha sesuka mu dengan dukungan kustomisasi penuh.\n\nApakah kamu ingin memainkan nya?\nKalau iya tolong pilih setuju untuk mengizinkan akses penyimpanan dan unduh sumber daya tambahan sekitar 70Mb an.\n\n! Jika tidak setuju maka game ini keluar !\n\n\n\n\nGacha Design Studio ini dibuat olah para penggemar Lunime")
@@ -222,10 +242,13 @@ if (agreementAccepted) {
             dialog,
             which ->
             dialog.dismiss()
+            GachaStudioLogger.log("Menghapus dialog sambutan :> ...")
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                 requestStoragePermission()
+                GachaStudioLogger.log("Archana Berry meminta akses nya kawan Un U ...")
             } else {
                 downloadResources()
+                GachaStudioLogger.log("Mengunduh sumber daya Gacha Design Studio...")
             }
         }
 
@@ -233,10 +256,13 @@ if (agreementAccepted) {
             dialog,
             which ->
             dialog.dismiss()
+            GachaStudioLogger.log("Kamu ga setuju yasudah aku pergi :v")
             finish()
+            GachaStudioLogger.log("Bye...")
         }
         builder.setCancelable(false)
         builder.show()
+        GachaStudioLogger.log("Membangun dialog...")
     }
 
     // Method untuk meminta izin akses penyimpanan
@@ -247,18 +273,23 @@ if (agreementAccepted) {
                     val intent = Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION)
                     intent.data = Uri.parse("package:$packageName")
                     startActivityForResult(intent, PERMISSION_REQUEST_CODE)
+                    GachaStudioLogger.log("Mengarahkan ke pengaturan untuk diminta izinkan AKU!")
                 } else {
                     downloadResources()
+                    GachaStudioLogger.log("Otw download yagesya...")
                 }
             } else {
                 if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
                     ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), PERMISSION_REQUEST_CODE)
+                    GachaStudioLogger.log("Aku ingin mengecek sekali lagi :3")
                 } else {
                     downloadResources()
+                    GachaStudioLogger.log("Otw download yagesya...")
                 }
             }
         } else {
             downloadResources()
+            GachaStudioLogger.log("Otw download yagesya...")
         }
     }
 
@@ -269,10 +300,12 @@ if (agreementAccepted) {
         progressBar = dialogView.findViewById(R.id.progressBar)
         progressDialog?.setView(dialogView)
         progressDialog?.setTitle("Sedang mengunduh sumber daya tambahan... :>")
+        GachaStudioLogger.log("Kawan aku sedang mengunduh sumber daya tambahan... :3")
         progressDialog?.setCancelable(false)
         progressDialog?.setCanceledOnTouchOutside(false)
         progressDialog?.show()
         DownloadResourcesTask().execute()
+        GachaStudioLogger.log("Mengeksekusi tugas mengunduh...")
     }
 
     // AsyncTask untuk mengunduh sumber daya tambahan
@@ -283,17 +316,20 @@ if (agreementAccepted) {
                 val url = URL(RESOURCE_URL)
                 val connection = url.openConnection() as HttpURLConnection
                 connection.connect()
+                GachaStudioLogger.log("Mengkoneksikan kembali... dari $url ke $connection")
 
                 val fileLength = connection.contentLength
 
                 val tempDir = File(TEMP_DIR_PATH)
                 if (!tempDir.exists()) {
                     tempDir.mkdirs()
+                    GachaStudioLogger.log("Membuat folder $tempDir")
                 }
 
                 val outputFile = File(ZIP_FILE_PATH)
                 if (outputFile.exists()) {
                     outputFile.delete()
+                    GachaStudioLogger.log("Menghapus zip...")
                 }
 
                 val input = BufferedInputStream(url.openStream())
@@ -308,18 +344,26 @@ if (agreementAccepted) {
                     total += count.toLong()
                     publishProgress((total * 100 / fileLength).toInt())
                     output.write(data, 0, count)
+                    GachaStudioLogger.log("Progres pengunduhan...")
                 }
 
                 output.flush()
                 output.close()
                 input.close()
 
-                unzip(ZIP_FILE_PATH, EXTRACTED_DIR_PATH!!)
-                moveExtractedFiles(SOURCE_DIR_PATH!!, DEST_DIR_PATH!!)
-                deleteTempDirectory()
+                if (unzip(ZIP_FILE_PATH, EXTRACTED_DIR_PATH!!)) {
+                    GachaStudioLogger.log("Mengekstrak sumber daya...")
+                }
+                if (moveExtractedFiles(SOURCE_DIR_PATH!!, DEST_DIR_PATH!!)) {
+                GachaStudioLogger.log("Memindahkan ke folder Lunime...")
+                }
+                if (deleteTempDirectory()) {
+                    GachaStudioLogger.log("Menghapus folder sementara beserta isi nya...")
+                }
 
                 true
             } catch (e: IOException) {
+                GachaStudioLogger.log("Yah kak aku mengalami kegagalan Tw T!")
                 e.printStackTrace()
                 false
             }
@@ -330,6 +374,7 @@ if (agreementAccepted) {
             progressDialog?.setTitle("Sedang mengekstrak sumber daya tambahan... :>")
             values[0]?.let {
                 progressBar?.progress = it
+                GachaStudioLogger.log("Progres pembaruan...")
             }
         }
 
@@ -339,20 +384,25 @@ if (agreementAccepted) {
             if (success) {
                 val builder = AlertDialog.Builder(this@GachaStudioMain)
                 builder.setTitle("Unduhan Berhasil")
+                GachaStudioLogger.log("Yey, dah berhasil kawan didownload Uw U")
                 builder.setMessage("Sumber daya sudah berhasil diekstrak! O wO")
+                GachaStudioLogger.log("Yey, sumber daya nya sudah berhasil di ekstrak Uw U")
                 builder.setPositiveButton("OK") {
                     dialog,
                     which ->
                     dialog.dismiss()
                     setAgreementAccepted(true)
                     openGachaDesignStudio()
+                    GachaStudioLogger.log("Saat nya ditunggu tunggu, membuka Gacha Studio Design...")
                 }
                 builder.setCancelable(false)
                 builder.show()
+                GachaStudioLogger.log("Memunculkan bangun dialog")
             } else {
                 val builder = AlertDialog.Builder(this@GachaStudioMain)
                 builder.setTitle("Unduhan Gagal")
                 builder.setMessage("Maaf, unduhan gagal. Silakan coba lagi nanti T vT.")
+                GachaStudioLogger.log("Yah kak aku mengalami kegagalan mengunduh Tw T!")
                 builder.setPositiveButton("OK") {
                     dialog,
                     which ->
@@ -361,6 +411,7 @@ if (agreementAccepted) {
                 }
                 builder.setCancelable(false)
                 builder.show()
+                GachaStudioLogger.log("Memunculkan bangun dialog")
             }
         }
     }
@@ -370,23 +421,30 @@ if (agreementAccepted) {
             val destDir = File(destDirectory)
             if (!destDir.exists()) {
                 destDir.mkdirs()
+                GachaStudioLogger.log("Membuat folder di: $destDir")
             }
             val zipIn = ZipInputStream(FileInputStream(zipFilePath))
             var entry: ZipEntry ? = zipIn.nextEntry
+            GachaStudioLogger.log("Memasang antrian unzip $entry")
             while (entry != null) {
                 val filePath = destDirectory + File.separator + entry.name
                 if (!entry.isDirectory) {
                     extractFile(zipIn, filePath)
+                    GachaStudioLogger.log("Mengekstrak file sumber daya $zipIn ke $filePath")
                 } else {
                     val dir = File(filePath)
                     dir.mkdirs()
+                    GachaStudioLogger.log("Membuat folder di $filePath")
                 }
                 zipIn.closeEntry()
                 entry = zipIn.nextEntry
+                GachaStudioLogger.log("Menutup antrian unzip $entry")
             }
             zipIn.close()
+            GachaStudioLogger.log("Menutup ekstrak sumber daya file")
         } catch (e: IOException) {
             e.printStackTrace()
+            GachaStudioLogger.log("Waduh kak, terjadi error  pas lagi ekstrak nih?", isError = true)
         }
     }
 
@@ -401,8 +459,10 @@ if (agreementAccepted) {
                 bos.write(bytesIn, 0, read)
             }
             bos.close()
+            GachaStudioLogger.log("Menutup pengekstrakan (BOS)...")
         } catch (e: IOException) {
             e.printStackTrace()
+            GachaStudioLogger.log("Yah kak error nih? (BOS)", isError = true)
         }
     }
 
@@ -414,6 +474,7 @@ if (agreementAccepted) {
             // Pastikan direktori tujuan sudah ada
             if (!destDir.exists()) {
                 destDir.mkdirs()
+                GachaStudioLogger.log("Memindahkan file hasil ekstrak dari $srcDir ke $destDir")
             }
 
             // Salin semua file dan direktori dari srcDir ke destDir
@@ -423,17 +484,21 @@ if (agreementAccepted) {
                 val destFile = File(destDir, relativePath.path)
                 if (file.isDirectory) {
                     destFile.mkdirs()
+                    GachaStudioLogger.log("Menyalin semua file dari $relativePath ke $destFile")
                 } else {
                     file.copyTo(destFile, overwrite = true)
+                    GachaStudioLogger.log("Mwehehe, file lama akan ku update!, KEMUNGKINAN BESAR DATA KEGANTI DENGAN BARU!", isError = true)
                 }
             }
 
             // Hapus direktori sumber setelah selesai menyalin
             if (srcDir.exists()) {
                 FileUtils.deleteDirectory(srcDir)
+                GachaStudioLogger.log("Aku menghapus folder ini $srcDir ...", isError = true)
             }
         } catch (e: IOException) {
             e.printStackTrace()
+            GachaStudioLogger.log("Yah kak terjadi kesalahan!", isError = true)
         }
     }
 
@@ -442,6 +507,7 @@ if (agreementAccepted) {
         try {
             val tempDir = File(TEMP_DIR_PATH)
             FileUtils.deleteDirectory(tempDir)
+            GachaStudioLogger.log("Menghapus folder sementara $tempDir agar luas memori nya", isError = true)
         } catch (e: IOException) {
             e.printStackTrace()
         }
@@ -548,11 +614,6 @@ private fun showResourceUpdateDialog() {
         .setNegativeButton("Nanti", null)
         .show()
 }
-    ///
-    
-    //3
-    
-    //3
     
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array < String > ,
         grantResults: IntArray) {
@@ -560,11 +621,13 @@ private fun showResourceUpdateDialog() {
         if (requestCode == PERMISSION_REQUEST_CODE) {
             if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 downloadResources()
+                GachaStudioLogger.log("Otw mengunduh ygy...")
             } else {
                 val builder = AlertDialog.Builder(this)
                 builder.setTitle("Izin Diperlukan")
                 builder.setMessage(
                     "Game ini memerlukan izin akses penyimpanan untuk melanjutkan. Izinkan dong akses penyimpanan nya melalui Pengaturan nya?, agar game ini bisa berjalan :v")
+                    GachaStudioLogger.log("Izinkan dulu atuh akses mengolah file nya?!")
                 builder.setPositiveButton("OK") {
                     dialogInterface,
                     i ->
@@ -581,6 +644,7 @@ private fun showResourceUpdateDialog() {
                 }
                 builder.setCancelable(false)
                 builder.show()
+                GachaStudioLogger.log("Membangun dialog...")
             }
         }
     }
@@ -591,8 +655,10 @@ private fun showResourceUpdateDialog() {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
                 if (Environment.isExternalStorageManager()) {
                     downloadResources()
+                    GachaStudioLogger.log("Otw mengunduh...")
                 } else {
                     Toast.makeText(this, "Kok gak diizinin?, jika ditolak game nya tidak dapat melanjutkan nya TwT.", Toast.LENGTH_SHORT).show()
+                    GachaStudioLogger.log("//Menendang anda keluar...", isError = true)
                     finish()
                 }
             }
